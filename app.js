12805ab6a1012140c4f7107a643941ec79698244
Jeffry503 <rodriguez.jefferson2000@gmail.com>
2023-07-21 09:15:38 -0400

    New Changes
    Background image and added some features with a menu as well

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const colorButton = document.getElementById('colorButton');
-
-colorButton.addEventListener('click', function() {
-  const randomColor = getRandomColor();
-  document.body.style.backgroundColor = randomColor;
-});
-
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "block";
+    }
 }
+
