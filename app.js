2a3492b3d7d3a9e64db5b301983df616cec17e23
Jeffry503 <rodriguez.jefferson2000@gmail.com>
2023-07-25 17:10:33 -0500

    Last edits

--- app.js
+++
@@ -1,9 +0,0 @@
-function toggleDropdown() {
-    var dropdownMenu = document.getElementById("dropdownMenu");
-    if (dropdownMenu.style.display === "block") {
-        dropdownMenu.style.display = "none";
-    } else {
-        dropdownMenu.style.display = "block";
-    }
-}
-
